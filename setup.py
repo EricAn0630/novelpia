4835249e4269ab076489f1c0b78035b279dcf4ce
Eric_An <72683362+EricAn0630@users.noreply.github.com>
2021-09-09 09:43:03 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,20 @@ import novelpia_python
 
 VERSION = novelpia_python.__version__
 
-setup(name="novelpia_python",version=VERSION,packages=find_packages(),author="Eric_An0630")
+def file(path: str) -> str:
+    with open('README.md', encoding='utf8') as f:
+        description = f.read()
+    return description
+
+setup(
+    name="novelpia_python",
+    version=VERSION,
+    packages=find_packages(),
+    author="Eric_An0630",
+    author_email="soochan06@naver.com",
+    url="https://github.com/EricAn0630/novelpia-python",
+    description="노벨피아 비공식 라이브러리",
+    long_description=file('README.md'),
+    long_description_content_type="text/markdown",
+    license="MIT License",python_reqires=">=3.6"
+)
