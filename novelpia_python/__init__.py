06dbb5f1bdbea763b279221728f7d118d4ed0dad
Eric_An <72683362+EricAn0630@users.noreply.github.com>
2021-09-09 10:28:20 +0900

    Update __init__.py

--- novelpia_python/__init__.py
+++ novelpia_python/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ['main', 'errors']
 
-__version__ = "0.0.1.b"
+__version__ = "0.0.1.c"
 
 from novelpia_python.main import Novelpia
 from novelpia_python.errors import NonExistNovel, InvalidSortType
